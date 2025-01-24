b312bc952d12cad73b445a3eb5c6fcde5ab478bc
dirums <yodaljit1@gmail.com>
2025-01-25 02:24:18 +0530

    updated notification setup

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,14 +2,13 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDFN-73p8zKVZbA0i5DtO215XzAb-xuGSE",
-  authDomain: "ammart-8885e.firebaseapp.com",
-  databaseURL: "https://ammart-8885e-default-rtdb.firebaseio.com",
-  projectId: "ammart-8885e",
-  storageBucket: "ammart-8885e.appspot.com",
-  messagingSenderId: "1000163153346",
-  appId: "1:1000163153346:web:4f702a4b5adbd5c906b25b",
-  measurementId: "G-L1GNL2YV61"
+    apiKey: "AIzaSyCjLZTCYuLpPI5WD9D0iSBA8SqD6GRHtNo",
+    authDomain: "mart-83e68.firebaseapp.com",
+    projectId: "mart-83e68",
+    storageBucket: "mart-83e68.firebasestorage.app",
+    messagingSenderId: "852103073977",
+    appId: "1:852103073977:web:c9e6436766f1392828cb68",
+    databaseURL: "...",
 });
 
 const messaging = firebase.messaging();
